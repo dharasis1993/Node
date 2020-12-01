edd4375d202d4abd48b5af9992ced18aed143e39
Dharasis Behera <b.dhara93@gmail.com>
2020-12-01 22:27:08 +0530

    automation

--- index.js
+++ index.js
@@ -23,3 +23,20 @@ require("./routes/customer.routes.js")(app);
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
 })
+var http = new XMLHttpRequest();
+var url = 'localhost:3000/customers';
+var params = '{
+    name: "Dharasis",
+    active: 1
+  }';
+http.open('POST', url, true);
+
+//Send the proper header information along with the request
+http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+http.onreadystatechange = function() {//Call a function when the state changes.
+    if(http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+    }
+}
+http.send(params);
